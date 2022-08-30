c0b4d7488152e61bf1c2e31c79df5588a6e4e0e4
Анна Слонова <53565314+AnnSonrisa@users.noreply.github.com>
2022-08-30 23:00:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ class Record:
         self.date = (
             dt.datetime.now().date() if
             not
+            # Строковые литералы принято выносить из кода на случай, 
+            # если вдруг поменяют ТЗ и нужно будет менять формат даты.
+            # Легче же формат поменять в одном месте, верно? :)
             date else dt.datetime.strptime(date, '%d.%m.%Y').date())
         self.comment = comment
 
@@ -15,31 +18,52 @@ class Calculator:
     def __init__(self, limit):
         self.limit = limit
         self.records = []
-
+    
+    # Для нетривиальных функций стоит написать пояснение.
+    # Полезно будет по этой теме изучить статью: 
+    # https://habr.com/ru/post/499358/
     def add_record(self, record):
         self.records.append(record)
 
+    # Давай еще добавим аннотации :)
+    # С ними код станет понятнее другим разработчикам.
     def get_today_stats(self):
         today_stats = 0
+        # 1) Record - это класс. Не имеет смысла в записях (self.records) его искать.
+        # Логичнее будет элемент последовательности назвать, например, record.
+        # 2) Попробуй здесь использовать list comprehension ("составление списков"). 
+        # Вместо того, чтобы проходить циклом по всем событиям, можно собрать отдельный список из только тех событий, 
+        # которые подходят нам по условию.
         for Record in self.records:
+            # Дату dt.datetime.now().date() лучше вынести из кода (как константу).
+            # По pep-8 константам нужно давать говорящие названия и именовать в верхнем регистре.
             if Record.date == dt.datetime.now().date():
+                # Сумму поможет оператор sum().
+                # В качестве аргумента получает любую перечислимую коллекцию (список, кортеж и т.д.)
+                # и возвращает сумму элементов.
                 today_stats = today_stats + Record.amount
         return today_stats
 
     def get_week_stats(self):
         week_stats = 0
+        # Константу лучше вынести из кода (так как она используется в нескольких методах),
+        # также назвать ее нужно в верхнем регистре.
         today = dt.datetime.now().date()
         for record in self.records:
             if (
+                # Попробуй использовать двойное неравенство :)
+                # Это упростит выражение.
                 (today - record.date).days < 7 and
                 (today - record.date).days >= 0
             ):
+                # Также попробуй использовать оператор sum(). 
                 week_stats += record.amount
         return week_stats
 
 
 class CaloriesCalculator(Calculator):
     def get_calories_remained(self):  # Получает остаток калорий на сегодня
+        # Лучше подсчет оставшихся калорий или денег вынести в отдельный метод родительского класса.
         x = self.limit - self.get_today_stats()
         if x > 0:
             return f'Сегодня можно съесть что-нибудь' \
